ab08bbb5ab90bc72fa895c6901d323f8439551ac
admin <admin@DESKTOP-02VQPLP>
2020-05-04 18:47:18 +0530

    commit

---
+++ Lab1-MicroService-DemoH2Database1/src/main/java/com/cg/DemoH2Database/DemoH2DatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.cg.DemoH2Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoH2DatabaseApplication.class, args);
+		System.out.println("Spring Boot Application Running...");
+	}
+
+}
